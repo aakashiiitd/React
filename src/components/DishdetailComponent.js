4862e5ec961b2d1225591348807b81074a662e75
Aakash Tanwar <aakash16215@iiitd.ac.in>
2020-06-16 19:51:23 +0530

    Controlled and Uncontrolled Form

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
         if (dish != null)
             return (
-                <div className="row">
                     <div className="col-12 col-md-5 m-1">
                         <Card>
                             <CardImg width="100%" src={dish.image} alt={dish.name} />
@@ -14,12 +14,10 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                             </CardBody>
                         </Card>
                     </div>
-                    <RenderComments dishComments={dish.comments} />
-                </div>
-            )
+            );
         else return (
             <div></div>
-        )
+        );
     }
 
     function RenderComments({dishComments}) {
@@ -48,7 +46,20 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
     const DishDetail = (props) => {
         return (
         	<div className="container">
-            	<RenderDish dish={props.dish} />
+        		<div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+            		<RenderDish dish={props.dish} />
+            		<RenderComments dishComments={props.comments} />
+            	</div>
         	</div>
         );
     }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
+import Contact from './ContactComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -12,15 +17,30 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        dishes: DISHES
+        dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
     };
   }
 
+
   render() {
+
+    const DishWithId = ({match}) => {
+      return(
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    }
 
     const HomePage = () => {
       return(
-          <Home />
+          <Home 
+            dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+            promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+            leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
       );
     }
 
@@ -30,6 +50,9 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
+          <Route exact path='/contactus' component={Contact} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
